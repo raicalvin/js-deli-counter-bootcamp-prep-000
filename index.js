d3b093349616f5d69a8436c4042ce178ca7e8c6f
Calvin Simran Rai <raicalvin@gmail.com>
2018-07-27 20:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, newName) {
 function nowServing(katzDeliLine) {
   let person = katzDeliLine.unshift();
   if (person) {
-    return person;
+    return 'Currently servering ' + person;
   }
   return 'There is nobody waiting to be served!';
 }
