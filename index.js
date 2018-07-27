faf3fe8bc1cceef41de88d3132b98590e66b86af
Calvin Simran Rai <raicalvin@gmail.com>
2018-07-27 20:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(newName);
+  return `Welcome, ${newName}. You are number ${katzDelieLine.length} in line.`;
 }
