5635e9df30fc40a0fe7630236c3fbc018933cd76
Calvin Simran Rai <raicalvin@gmail.com>
2018-07-27 20:54:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
+  if (line.length === 0) {
+    return 'The line is currently empty.';
+  }
   let lineString = 'The line is currently: ';
   for (let i = 0; i < line.length; i++) {
     lineString += (i + 1) + '. ' + line[i];
